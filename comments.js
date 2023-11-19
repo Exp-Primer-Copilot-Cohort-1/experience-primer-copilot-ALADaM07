567e7aaf1887ae7d7197c7f950dffc656bcfcc64
Mohamad Alamin <alamin.mohamad.a0@gmail.com>
2023-11-19 13:28:42 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,41 @@
+// Create web server 
+// 1. Get all comments
+// 2. Get comment by id
+// 3. Create new comment
+// 4. Update comment by id
+// 5. Delete comment by id
 
-const http = require('http');
+var express = require("express");
+var router = express.Router();
+var Comment = require("../models/comment");
+var mongoose = require("mongoose");
 
-// Create a server object
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send a response
-  res.end('Hello, World!');
+// 1. Get all comments
+router.get("/", function(req, res) {
+    Comment.find({}, function(err, comments) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(comments);
+        }
+    });
 });
 
-// Start the server
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// 2. Get comment by id
+router.get("/:id", function(req, res) {
+    Comment.findById(req.params.id, function(err, comment) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(comment);
+        }
+    });
 });
+
+// 3. Create new comment
+router.post("/", function(req, res) {
+    var newComment = new Comment({
+        _id: new mongoose.Types.ObjectId(),
+// Import modules     
+const express = require('express');
+const router = express.Router();
